c2b62a210d27a0b5890e39c9de030059a31759da
Aleksandr Pimanov <pimanov.aleksandr00@mail.ru>
2022-09-02 18:20:53 +0400

    Initial Commit

---
+++ PhotoScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoScreen
+//
+//  Created by новый пользователь on 02.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
